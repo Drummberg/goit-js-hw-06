110f193121388157bd04c476568f4dd1db7a4e91
Drummberg <drummberg@gmail.com>
2022-01-11 17:45:10 +0200

    Create task-02.js

---
+++ js/task-02.js
@@ -0,0 +1,20 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const mainEl = document.querySelector("#ingredients");
+
+const elementsEl = ingredients.map((ingredient) => {
+    const ingredientsEl = document.createElement('li');
+    ingredientsEl.textContent = ingredient;
+    ingredientsEl.classList.add('item');
+
+    return ingredientsEl;
+});    
+console.log(elementsEl);
+mainEl.append(...elementsEl);
