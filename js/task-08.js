3792026a2a661028e56068b89f01bed88d4781f8
Drummberg <drummberg@gmail.com>
2022-01-15 20:54:28 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,6 +1,27 @@
 const form = document.querySelector('.login-form');
- form.addEventListener('submit', submitForm )
+form.addEventListener('submit', submitForm);
 
 function submitForm(event) {
     event.preventDefault();
- }
+
+      const {
+        elements: { email, password }
+        } = event.currentTarget;
+    
+    if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+    };
+
+    const formElements = event.currentTarget.elements;
+
+        const mail = formElements.email.value;
+    const passwordUser = formElements.password.value;
+
+    const formData = { mail, passwordUser };
+
+     
+
+    event.currentTarget.reset();
+    
+    console.log(formData);
+};
