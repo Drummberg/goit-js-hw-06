d1b3958de36ae2acc6f3aad93535607e803d8fd4
Drummberg <drummberg@gmail.com>
2022-01-15 15:57:06 +0200

    Update task-07.js

---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+const inputRef = document.querySelector('#font-size-control');
+const spanRef = document.querySelector('#text');
+
+
+inputRef.addEventListener('input', (event)=> {
+    spanRef.style.fontSize = `${event.currentTarget.value}px`;
+});
+
