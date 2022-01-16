af2b5076d6130cef8c752cbf86488452158c06b2
Drummberg <drummberg@gmail.com>
2022-01-17 01:21:42 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,9 +1,36 @@
-const input = document.querySelector('input');
+const inputEl = document.querySelector('#controls input');
 const btnCreate = document.querySelector('[data-create]');
 const btnDestroy = document.querySelector('[data-destroy]');
 const newDivBoxes = document.querySelector('#boxes');
 
-function createBoxes(amount) {
-    
 
+inputEl.addEventListener('input', () => {
+    
+    return btnCreate.addEventListener       ('click', createBoxes);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+
+function createBoxes(amount) {
+    // amount = inputEl.value;
+    let divWidth = 30;
+    let divHeight = 30;
+    const divs = document.createElement("div");
+    divs.length;
+    divs.style.width = divWidth + 'px';
+    divs.style.height = divHeight + 'px';
+    divs.style.color = getRandomHexColor();
+    newDivBoxes.append(divs);
+    console.log(divs.style.color);
+};
+
+btnDestroy.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+    const deleteDivs = document.querySelector('#boxes div');
+    deleteDivs.remove();
 }
+
