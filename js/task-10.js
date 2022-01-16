c97f1a9815d2af4d3c71483fd9f5b6934746a6f7
Drummberg <drummberg@gmail.com>
2022-01-16 13:20:41 +0200

    Create task-10.js

---
+++ js/task-10.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('input');
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+const newDivBoxes = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+    
+
+}
