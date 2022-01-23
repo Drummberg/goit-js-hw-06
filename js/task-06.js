687f6f1948f08eb7ff337f9f7921e5c14f701508
Drummberg <drummberg@gmail.com>
2022-01-23 23:01:36 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,9 +3,17 @@ const dataRef = document.querySelector('[data-length="6"]');
 const numberData = Number(dataRef.dataset.length);
 
 inputRef.addEventListener("blur", (event) => {
-    if (event.currentTarget.value.length === numberData) {
-        return inputRef.setAttribute('class', 'valid');
-    } else { return inputRef.setAttribute('class', 'invalid') };
-   
+    const resultClass = event.currentTarget.value.length === numberData ? inputRef.setAttribute('class', 'valid') : inputRef.setAttribute('class', 'invalid');
+
 });
+
+
+
+
+// inputRef.addEventListener("blur", (event) => {
+//     if (event.currentTarget.value.length === numberData) {
+//         return inputRef.setAttribute('class', 'valid');
+//     } else { return inputRef.setAttribute('class', 'invalid') };
+   
+// });
 
