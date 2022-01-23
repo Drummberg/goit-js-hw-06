2b005b5b5c6298f649dc6e4a75cbead8f86b290d
Drummberg <drummberg@gmail.com>
2022-01-23 23:01:17 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,25 @@
 
-const listEl = document.querySelectorAll(".item");
-const totals = listEl.length;
-console.log(`Number of categories: ${totals}`);
+// const listEl = document.querySelectorAll(".item");
+// const totals = listEl.length;
+// console.log(`Number of categories: ${totals}`);
 
-for (let i = 0; i < totals; i += 1){
-    const categoriesNameEl = listEl[i].firstElementChild.textContent;
-    console.log(`Category: ${categoriesNameEl}`);    
+// for (let i = 0; i < totals; i += 1){
+//     const categoriesNameEl = listEl[i].firstElementChild.textContent;
+//     console.log(`Category: ${categoriesNameEl}`);
     
-    const ChildElements = listEl[i].lastElementChild;
-    const totalsChildEl = ChildElements.querySelectorAll('li')
-    console.log(`Elements: ${totalsChildEl.length}`);
-}
+//     const ChildElements = listEl[i].lastElementChild;
+//     const totalsChildEl = ChildElements.querySelectorAll('li')
+//     console.log(`Elements: ${totalsChildEl.length}`);
+// }
 
+const list = document.querySelector('#categories');
+const items = list.querySelectorAll('.item');
+console.log(`В списке ${items.length} категории:`);
+items.forEach(node => {
+const h2 = node.querySelector('h2');
+const nodeItems = node.querySelectorAll('li');
+console.log(` - категория: ${h2.textContent} (количество элементов: ${nodeItems.length})`);
+});
 
 
 
