a51f0a365a755f3dd68d0e304b114464d0d6a09a
Drummberg <drummberg@gmail.com>
2022-01-23 23:04:12 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -4,11 +4,19 @@ const spanRef = document.querySelector('#name-output');
 searchRef.addEventListener("input", boo);
  
 function boo(event) {
-    if (searchRef.value !== '') {
-    return spanRef.textContent = searchRef.value;
-    }
-    else {
-        return spanRef.textContent = 'Anonymous';
-    }
-
+    const resultContent = searchRef.value !== ''
+    ? spanRef.textContent = searchRef.value :
+    spanRef.textContent = 'Anonymous';
+ 
 };
+
+
+// function boo(event) {
+//     if (searchRef.value !== '') {
+//     return spanRef.textContent = searchRef.value;
+//     }
+//     else {
+//         return spanRef.textContent = 'Anonymous';
+//     }
+
+// };
